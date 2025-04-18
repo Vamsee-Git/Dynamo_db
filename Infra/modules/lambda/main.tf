fde7dc68b64d1dc78f4eee95546e5bcb68e3fcab
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-18 17:51:57 +0530

    Update main.tf

--- Infra/modules/lambda/main.tf
+++ Infra/modules/lambda/main.tf
@@ -14,7 +14,7 @@ resource "aws_iam_role" "lambda_exec_role" {
 
 resource "aws_lambda_function" "user_data_function" {
   function_name = "user_data_function"
-  handler       = "index.lambda_handler"
+  handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
   role          = aws_iam_role.lambda_exec_role.arn
 
