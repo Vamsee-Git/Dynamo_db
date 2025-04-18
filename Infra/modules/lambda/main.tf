355af1bdd7a17f4d696c64e9f8f91ef3326aaf34
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-18 16:36:39 +0530

    Update main.tf

--- Infra/modules/lambda/main.tf
+++ Infra/modules/lambda/main.tf
@@ -25,48 +25,6 @@ resource "aws_lambda_function" "user_data_function" {
   }
 
   filename = "lambda_function.zip"
-
-  code {
-    zip_file = <<EOF
-import boto3
-import json
-
-def lambda_handler(event, context):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('user_data')
-    
-    if event['httpMethod'] == 'POST':
-        body = json.loads(event['body'])
-        user_id = body['user_id']
-        user_data = body['user_data']
-        
-        table.put_item(
-            Item={
-                'user_id': user_id,
-                'user_data': user_data
-            }
-        )
-        
-        return {
-            'statusCode': 200,
-            'body': json.dumps('User added successfully!')
-        }
-    
-    elif event['httpMethod'] == 'GET':
-        user_id = event['queryStringParameters']['user_id']
-        
-        response = table.get_item(
-            Key={
-                'user_id': user_id
-            }
-        )
-        
-        return {
-            'statusCode': 200,
-            'body': json.dumps(response.get('Item', {}))
-        }
-EOF
-  }
 }
 
 output "lambda_function_arn" {
