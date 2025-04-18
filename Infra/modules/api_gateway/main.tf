eff96ef6de0b6145f76781590c0fbf86e8b341c3
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-18 15:54:23 +0530

    Create main.tf

---
+++ Infra/modules/api_gateway/main.tf
@@ -0,0 +1,39 @@
+resource "aws_api_gateway_rest_api" "user_api" {
+  name        = "user_api"
+  description = "API for managing user data"
+}
+
+resource "aws_api_gateway_resource" "user_resource" {
+  rest_api_id = aws_api_gateway_rest_api.user_api.id
+  parent_id   = aws_api_gateway_rest_api.user_api.root_resource_id
+  path_part   = "user"
+}
+
+resource "aws_api_gateway_method" "get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.user_api.id
+  resource_id   = aws_api_gateway_resource.user_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+  integration {
+    type               = "AWS_PROXY"
+    integration_http_method = "POST"
+    uri                = var.lambda_function_arn
+  }
+}
+
+resource "aws_api_gateway_method" "post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.user_api.id
+  resource_id   = aws_api_gateway_resource.user_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+  integration {
+    type               = "AWS_PROXY"
+    integration_http_method = "POST"
+    uri                = var.lambda_function_arn
+  }
+}
+
+variable "lambda_function_arn" {
+  description = "The ARN of the Lambda function"
+  type        = string
+}
