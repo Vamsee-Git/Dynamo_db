fde63382bf72b330664bb409fff2dfd667ad027e
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-21 08:21:35 +0530

    Update main.tf

--- Infra/modules/lambda/main.tf
+++ Infra/modules/lambda/main.tf
@@ -17,7 +17,7 @@ resource "aws_lambda_function" "user_data_function" {
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
   role          = aws_iam_role.lambda_exec_role.arn
-  timeout  = 10
+  timeout       = 10
   environment {
     variables = {
       DYNAMODB_TABLE = var.dynamodb_table
