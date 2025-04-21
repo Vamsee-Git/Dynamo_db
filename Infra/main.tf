874f3967ac86778b2b7d2955c813c67f9c090448
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-21 18:21:43 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,6 +1,14 @@
 provider "aws" {
   region = var.region
 }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.94.1"
+    }
+  }
+}
 
 module "dynamodb" {
   source = "./modules/dynamodb"
