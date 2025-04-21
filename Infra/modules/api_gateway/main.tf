ab52434133b9ea981f8c362290581d7fcb2e6dce
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-21 07:18:35 +0530

    Update main.tf

--- Infra/modules/api_gateway/main.tf
+++ Infra/modules/api_gateway/main.tf
@@ -1,3 +1,64 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_dynamodb_table" "user_data" {
+  name           = "user_data"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "user_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "user_data"
+  }
+}
+
+resource "aws_lambda_function" "user_data_function" {
+  function_name = "user_data_function"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.8"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+
+  filename = "lambda_function_payload.zip"
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+
+  inline_policy {
+    name = "lambda_policy"
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [{
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem"
+        ],
+        Effect = "Allow",
+        Resource = aws_dynamodb_table.user_data.arn
+      }]
+    })
+  }
+}
+
 resource "aws_api_gateway_rest_api" "user_api" {
   name        = "user_api"
   description = "API for managing user data"
@@ -24,21 +85,21 @@ resource "aws_api_gateway_method" "post_method" {
 }
 
 resource "aws_api_gateway_integration" "get_integration" {
-  rest_api_id = aws_api_gateway_rest_api.user_api.id
-  resource_id = aws_api_gateway_resource.user_resource.id
-  http_method = aws_api_gateway_method.get_method.http_method
-  type        = "AWS_PROXY"
-  integration_http_method = "POST"
-  uri         = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
+  rest_api_id             = aws_api_gateway_rest_api.user_api.id
+  resource_id             = aws_api_gateway_resource.user_resource.id
+  http_method             = aws_api_gateway_method.get_method.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "GET"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
 }
 
 resource "aws_api_gateway_integration" "post_integration" {
-  rest_api_id = aws_api_gateway_rest_api.user_api.id
-  resource_id = aws_api_gateway_resource.user_resource.id
-  http_method = aws_api_gateway_method.post_method.http_method
-  type        = "AWS_PROXY"
+  rest_api_id             = aws_api_gateway_rest_api.user_api.id
+  resource_id             = aws_api_gateway_resource.user_resource.id
+  http_method             = aws_api_gateway_method.post_method.http_method
+  type                    = "AWS_PROXY"
   integration_http_method = "POST"
-  uri         = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
@@ -50,6 +111,14 @@ resource "aws_api_gateway_deployment" "api_deployment" {
     aws_api_gateway_integration.get_integration,
     aws_api_gateway_integration.post_integration
   ]
+}
+
+resource "aws_lambda_permission" "apigateway_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.user_data_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.user_api.id}/*/POST/user"
 }
 
 variable "lambda_function_arn" {
